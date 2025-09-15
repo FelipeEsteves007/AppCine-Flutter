00f61165ff89db7a63547c354a693dc2a99317a6
unknown <felipeestevesdepaula@gmail.com>
2025-09-15 15:42:34 -0300

    add searchScreen and mvmm

--- lib/ui/components/buttons.dart
+++ lib/ui/components/buttons.dart
@@ -1,19 +1,35 @@
 import 'package:flutter/material.dart';
 
 class PrimaryButton extends StatelessWidget {
-  const PrimaryButton({super.key});
+  final String text;
+  final IconData? icon;
+  final Function onTap;
+
+  const PrimaryButton({super.key, required this.text, this.icon, required this.onTap});
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: () {},
+      onTap: () {
+        onTap();
+      },
       child: Ink(
+        padding: const EdgeInsets.symmetric(vertical: 19, horizontal: 26),
         decoration: BoxDecoration(
           color: Color(0xFFB370FF),
           borderRadius: BorderRadius.circular(50),
         ),
         child: Row(
-          children: [Text("Quero começar"), Icon(Icons.arrow_forward)],
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              text , style: TextStyle(
+                color: Color(0xFF1d0E44),
+                fontWeight: FontWeight.w700,
+              ),),
+            icon != null ? Icon(icon, color: Color(0xFF1d0E44)) : Container(),
+          ],
         ),
       ),
     );
